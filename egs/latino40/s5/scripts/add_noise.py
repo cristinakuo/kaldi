7624937a10e8570c5bdb78e6babaa609d66c8b35
Cristina Kuo <cristinakuo@Cristinas-Air.fibertel.com.ar>
2023-08-05 14:36:31 -0300

    Improvements

--- egs/latino40/s5/scripts/add_noise.py
+++ egs/latino40/s5/scripts/add_noise.py
@@ -13,31 +13,16 @@ import IPython.display as ipd
 import soundfile as sf
 
 
-def add_noise(signal_array, noisy_array, snr):
-    noise_torch = torch.from_numpy(noisy_array).float()
-    signal_torch = torch.from_numpy(signal_array).float()
-    noise_len = len(noise_torch)
-
-    signal_len = len(signal_torch)
-    random_start = random.randint(0, noise_len-signal_len)
-    
-    signal_power = signal_torch.norm(p=2)
-    noise_short = noise_torch[random_start:random_start+signal_len]
-    noise_power = noise_short.norm(p=2)
-    scale = snr * noise_power / signal_power
-    print("Scale", scale)
-    noisy_signal_torch = scale * signal_torch + noise_short
-    noisy_signal_array = noisy_signal_torch.numpy()
-    
-    return noisy_signal_array
-
-def add_noise_torch(signal_array, noise_array, snr_db):
+def add_noise(signal_array: np.array, noise_array: np.array, snr_db: int) -> np.array:
+    signal_array = torch.from_numpy(signal_array).float()
+    noise_array = torch.from_numpy(noise_array).float()
+    snr_db = torch.tensor(snr_db)
     signal_len = len(signal_array)
     noise_len = len(noise_array)
     random_start = random.randint(0, noise_len-signal_len)
     noise_short = noise_array[random_start:random_start+signal_len]
     
-    return F.add_noise(signal_array, noise_short, snr_db)
+    return F.add_noise(signal_array, noise_short, snr_db).numpy()
 
 
 def main():
@@ -51,26 +36,15 @@ def main():
     # TODO: path to a folder
 
     # Read noise
-    snr = 10**(args.snr_db/20)
     print(f"Adding noise to all data... SNR_DB={args.snr_db}")
+    snr_db = args.snr_db
     noise_array = SPHFile(args.noise_file).content
     input_array = SPHFile(args.input_file).content
     
-    # Using torch audio
-    input_array = torch.from_numpy(input_array).float()
-    noise_array = torch.from_numpy(noise_array).float()
-    snr_db = torch.tensor(args.snr_db)
-    torchaudio_result = add_noise_torch(input_array, noise_array, snr_db)
-    #torchaudio_result = np.int16(torchaudio_result * 32767)
-
-    #custom_result = add_noise(input_array, noise_array, snr)
-    #custom_array = np.int16(custom_array * 32767)
-    result = torchaudio_result.numpy()
+    result = add_noise(input_array, noise_array, snr_db)
     result = result/np.max(result)
     
-    #write_to_wav(args.output_file, noisy_array, 16000)
     sf.write(args.output_file, result, 16000, 'PCM_16')
-    #write(args.output_file, 16000, result)
     
     print("Done")
 
